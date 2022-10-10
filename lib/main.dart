333fae7b7a444eabdccdacbf40c75cfbb4ea4c4e
GiorgiKumelashvili <giorgi.kumelashvili21@gmail.com>
2022-10-10 21:34:03 +0400

    added export of unit

--- lib/main.dart
+++ lib/main.dart
@@ -3,4 +3,5 @@ library flutter_form_validation;
 export './src/config/vvo_config.dart';
 export './src/enum/main.dart';
 export './src/utils/either.dart';
+export './src/utils/unit.dart';
 export './src/validation/main.dart';
